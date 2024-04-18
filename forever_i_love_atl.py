727212396da25e6e727bf41e813e59129a75c510
gluke <gluke@momnt.com>
2024-04-18 13:35:17 -0400

    Deleted Files

--- forever_i_love_atl.py
+++
@@ -1,9 +0,0 @@
-a_city = "atlanta"
-a_state = "georgia"
-a_country = "united states"
-
-def sentence_builder(city, state, country):
-    print("this function will run")
-    return "forever i love " + city + ", " + state + ", " + country
-
-print(sentence_builder(a_city, a_state, a_country))
