bdca46a82ede9f9a9ad069dc21a63feb7e26482d
gluke <gluke@momnt.com>
2024-04-18 13:28:40 -0400

    Deleted files

--- test_script.py
+++
@@ -1,9 +0,0 @@
-print("Chris Bradford loves to make music but hasn't released any but said he has")
-print("I love this job")
-
-def basketball_stats(points, rebounds, assists):
-    print(f"points:{points}")
-    print(f"rebounds:{rebounds}")
-    print(f"assists:{assists}")
-
-basketball_stats(27.4, 7.8, 6.4)
