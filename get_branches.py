a9dd17a487cfb66deaa607eeae0efbf365c66ddb
Christopher Bradford <cbradford@momnt.com>
2024-04-22 14:06:38 -0400

    First request for branches

--- get_branches.py
+++ get_branches.py
@@ -4,3 +4,12 @@
 # Check PR is up to date with main branch
 # Merge PR
 # Update next PR in list to be merged
+
+import requests
+
+pr_list = [6, 7]
+url = "repos/glukerm/Scripts/branches"
+
+r = requests.get(f"https://api.github.com/{url}")
+print(r.status_code)
+print(r.json())
