ac23b408decdc7cfa3313cdd32bd20db7f06268a
Christopher Bradford <cbradford@momnt.com>
2024-04-22 13:33:47 -0400

    Script to check merge status

---
+++ get_branches.py
@@ -0,0 +1,6 @@
+# Store list of PRs to merge
+# Use GitHub API to get list of branches
+# Update first PR in list to be merged
+# Check PR is up to date with main branch
+# Merge PR
+# Update next PR in list to be merged
